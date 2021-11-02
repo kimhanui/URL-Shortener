2accd21f7b9a35a245b27d00ad439d46ceb8f151
kimhanui <30483337+kimhanui@users.noreply.github.com>
2021-11-02 22:27:58 +0900

    feat : 홈 page, api 생성 [#1]
    - 홈 page, api 생성
    
    Resolved #1

--- src/main/java/com/smile/urlshortener/web/UrlShortenerController.java
+++ src/main/java/com/smile/urlshortener/web/UrlShortenerController.java
@@ -1,7 +1,18 @@
 package com.smile.urlshortener.web;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Slf4j
 @RestController
 public class UrlShortenerController {
+
+    @GetMapping("/shorten")
+    public String shorten(@ModelAttribute PushUrlDto pushUrl) {
+        log.info(pushUrl.getUrl());
+        return "shorten-url";
+    }
 }
